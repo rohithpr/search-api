672048d378bf25b3e72d9b4afedcb6e79085792d
rohithpr <praroh2@gmail.com>
2015-07-30 09:35:18 +0530

    Added endpoint - search

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, redirect, url_for
 from pws import Bing, Google
+
+import random
 
 app = Flask(__name__)
 
@@ -17,8 +19,58 @@ def root():
             'py-web-search': 'https://github.com/rohithpr/py-web-search/issues',
             'search-api': 'https://github.com/rohithpr/search-api/issues',
         },
+        'end_points': {
+            'search': '/search',
+        }
     }
     return jsonify(project_information)
 
+
+def argument_validator(request):
+    errors = {}
+
+    query = request.args.get('q')
+    print('q')
+    if query is None:
+        errors['q'] = 'Parameter q is required'
+
+    num = request.args.get('num') or 10
+    try:
+        num = int(num)
+    except:
+        errors['num'] = 'Parameter num should be an integer'
+
+    start = request.args.get('start') or 0
+    try:
+        start = int(start)
+    except:
+        errors['start'] = 'Parameter start should be an integer'
+
+    recent = request.args.get('recent')
+    if recent and recent not in ['h', 'd', 'w', 'y']:
+        errors['recent'] = 'Parameter recent should be h, d, w, or y'
+
+    engine = request.args.get('engine') or random.choice(['bing', 'google'])
+    engine = engine.lower()
+    if engine not in ['bing', 'google']:
+        errors['engine'] = 'Parameter engine should be bing or google'
+    elif engine == 'bing':
+        engine = Bing
+    elif engine == 'google':
+        engine = Google
+
+    arguments = {'query': query, 'num': num, 'start': start, 'sleep': False, 'recent': recent}
+    return errors, arguments, engine
+
+@app.route('/search/')
+def search():
+    errors, arguments, engine = argument_validator(request)
+    print(errors, arguments, engine)
+    if len(errors.keys()) > 0:
+        return jsonify(errors)
+    else:
+        result = engine.search(**arguments)
+        return jsonify(result)
+
 if __name__ == '__main__':
-    app.run(threaded=True)
+    app.run(host='0.0.0.0', threaded=True, debug=True)
